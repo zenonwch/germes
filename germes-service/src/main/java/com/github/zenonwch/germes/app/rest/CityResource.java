31d38d1ac2d8945e0f75192b968b09fda5e9d9bf
Andrey Veshtard <andrey.veshtard@ctco.lv>
2018-02-08 22:33:48 +0200

    added first REST endpoint /cities

---
+++ germes-service/src/main/java/com/github/zenonwch/germes/app/rest/CityResource.java
@@ -0,0 +1,25 @@
+package com.github.zenonwch.germes.app.rest;
+
+import com.google.common.collect.Lists;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+import java.util.List;
+
+
+/**
+ * {@link CityResource} is REST web-service that handles city-related requests
+ *
+ * @author admin
+ */
+@Path("cities")
+public class CityResource {
+
+    @GET
+    @Produces(MediaType.APPLICATION_JSON)
+    public List<String> findCities() {
+        return Lists.newArrayList("Riga", "Vilnius", "Minsk");
+    }
+}
